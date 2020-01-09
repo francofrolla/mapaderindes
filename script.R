a4af22ef95b1d8a3f99a8f58cb47beda2bed880b
Franco Frolla <frolla.franco@inta.gob.ar>
2020-01-09 14:54:42 -0300

    Update script.R

--- script.R
+++ script.R
@@ -257,7 +257,7 @@ exportar<-function(nombre,detalleslote,unidad_de_cosecha,Lote,version_simplifica
           #plot(h, col=k,main=paste("Distribucion de rendimiento, Lote:", Lote),xlab= paste("Rendimiento",unidad_de_cosecha),ylab="Frecuencia")
 
 
-
+          ruta<-getwd()
           nombre1<-paste("Mapa de rendimiento-",Lote,".pdf",sep= "")
           pdf(file= paste(ruta,"/",nombre1,sep=""))
           #par(mar = c(2,2,2,2)
@@ -397,5 +397,27 @@ exportar<-function(nombre,detalleslote,unidad_de_cosecha,Lote,version_simplifica
       hacerkml()
       
           
+}
+
+
+addImg <- function(
+  obj, # an image file imported as an array (e.g. png::readPNG, jpeg::readJPEG)
+  x = NULL, # mid x coordinate for image
+  y = NULL, # mid y coordinate for image
+  width = NULL, # width of image (in x coordinate units)
+  interpolate = TRUE # (passed to graphics::rasterImage) A logical vector (or scalar) indicating whether to apply linear interpolation to the image when drawing. 
+){
+  if(is.null(x) | is.null(y) | is.null(width)){stop("Must provide args 'x', 'y', and 'width'")}
+  USR <- par()$usr # A vector of the form c(x1, x2, y1, y2) giving the extremes of the user coordinates of the plotting region
+  PIN <- par()$pin # The current plot dimensions, (width, height), in inches
+  DIM <- dim(obj) # number of x-y pixels for the image
+  ARp <- DIM[1]/DIM[2] # pixel aspect ratio (y/x)
+  WIDi <- width/(USR[2]-USR[1])*PIN[1] # convert width units to inches
+  HEIi <- WIDi * ARp # height in inches
+  HEIu <- HEIi/PIN[2]*(USR[4]-USR[3]) # height in units
+  rasterImage(image = obj, 
+    xleft = x-(width/2), xright = x+(width/2),
+    ybottom = y-(HEIu/2), ytop = y+(HEIu/2), 
+    interpolate = interpolate)
 }
 
